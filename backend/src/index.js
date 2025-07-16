c0f983e0cfbaf518c2dbfa64382ce84c6a838601
kimtechnos <franciskimani559@gmail.com>
2025-07-16 15:14:10 +0300

    feat: completed backend with messaging routes, controllers, and user sidebar logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.contrller.js";
+const router = express.Router();
+// Get users for sidebar
+router.get("/users", protectRoute, getUsersForSidebar);
+// Get messages between two users
+router.get("/:id", protectRoute, getMessages);
+// Send a message to a user
+router.post("/send/:id", protectRoute, sendMessage);
+export default router;
